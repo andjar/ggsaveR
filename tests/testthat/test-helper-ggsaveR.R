3824f736146a16ba90b653b37d721d03e55b2dab
Anders Hagen Jarmund <anders.h.jarmund@ntnu.no>
2025-07-16 01:46:00 +0200

    fix

--- tests/testthat/test-helper-ggsaveR.R
+++ tests/testthat/test-helper-ggsaveR.R
@@ -1,5 +1,6 @@
 # A standard plot object to use in all tests
-p <- ggplot2::ggplot(mtcars, aes(mpg, wt)) + ggplot2::geom_point()
+p <- ggplot2::ggplot(mtcars, ggplot2::aes(mpg, wt)) +
+  ggplot2::geom_point()
 
 # A standard creator name for tests
 test_creator <- "Test Author, ggsaveR"
